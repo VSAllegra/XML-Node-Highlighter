b50f86ce780a12d749bbcf47b7c83ea97674047c
Vinny Allegra <vinnyallegra@gmail.com>
2022-09-01 13:42:45 -0400

    Added Notes For First Solution Method

---
+++ Highlighter.java
@@ -0,0 +1,11 @@
+public class Highlighter
+ {
+    /**
+     * Method I
+     * 
+     * Parse XML file with XML Java Library, Change Node Property, "selected" to "true"
+     * 
+     * Convert new XML Files into PNGS -> https://stackoverflow.com/questions/4905662/create-a-graph-image-png-jpg-from-an-xml-file-with-java
+     * 
+     */
+}
