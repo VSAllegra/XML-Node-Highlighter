7bf49802f4fc858ca278ce51ec06f5bd9b2ab848
Vinny Allegra <vinnyallegra@gmail.com>
2022-09-02 12:10:09 -0400

    Added Base Imports, and XML parsing code, tested on on of the xml files

--- Highlighter.java
+++ Highlighter.java
@@ -1,11 +1,29 @@
+import javax.xml.stream.*;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+
 public class Highlighter
- {
-    /**
-     * Method I
-     * 
-     * Parse XML file with XML Java Library, Change Node Property, "selected" to "true"
-     * 
-     * Convert new XML Files into PNGS -> https://stackoverflow.com/questions/4905662/create-a-graph-image-png-jpg-from-an-xml-file-with-java
-     * 
-     */
+ {  
+
+    // Code Refactored From:  https://stackoverflow.com/questions/5059224/which-is-the-best-library-for-xml-parsing-in-java 
+    public static void parse(String filename) throws XMLStreamException, IOException {
+        try (FileInputStream fis = new FileInputStream(filename)) {
+            XMLInputFactory xmlInFact = XMLInputFactory.newInstance();
+            XMLStreamReader reader = xmlInFact.createXMLStreamReader(fis);
+            while(reader.hasNext()) {
+                reader.next(); // do something here
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        try{
+        parse("com.dropbox.android.xml");
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+    }
+   
 }
