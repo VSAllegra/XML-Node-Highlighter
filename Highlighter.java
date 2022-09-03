22db68c53e756646f8618006e254d3b55eb51b1e
Vinny Allegra <vinnyallegra@gmail.com>
2022-09-03 12:09:45 -0400

    Removed Unecessary Library Imports

--- Highlighter.java
+++ Highlighter.java
@@ -1,16 +1,13 @@
 import org.w3c.dom.*;
-import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
-import javax.xml.transform.stream.StreamSource;
 import java.io.*;
 
 public class Highlighter
