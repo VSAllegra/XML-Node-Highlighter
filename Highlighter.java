d777f54cf988a302101cab68cabf8eb1115b623c
Vinny Allegra <vinnyallegra@gmail.com>
2022-09-03 12:09:02 -0400

    Refactored DOM Parser Changes Selected to True

--- Highlighter.java
+++ Highlighter.java
@@ -1,7 +1,16 @@
 import org.w3c.dom.*;
+import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
 import java.io.*;
 
 public class Highlighter
@@ -31,9 +40,29 @@ public class Highlighter
                 }   
             }
         }
+        FileOutputStream output = new FileOutputStream("modified.xml");
+        writeXml(doc, output);
     }catch(Exception e){
         e.printStackTrace();
     }
+}
+
+private static void writeXml(Document doc,  OutputStream output)    throws TransformerException, UnsupportedEncodingException {
+
+    TransformerFactory transformerFactory = TransformerFactory.newInstance();
+
+
+    Transformer transformer = transformerFactory.newTransformer();
+
+    // pretty print
+    transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+    transformer.setOutputProperty(OutputKeys.STANDALONE, "no");
+
+    DOMSource source = new DOMSource(doc);
+    StreamResult result = new StreamResult(output);
+
+    transformer.transform(source, result);
+
     }
 }
 
