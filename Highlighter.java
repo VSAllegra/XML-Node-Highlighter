f787126f7d1cbdfd528bf1712e260719423057d0
Vinny Allegra <vinnyallegra@gmail.com>
2022-09-03 11:51:20 -0400

    Changed Parser From XPath to DOM

--- Highlighter.java
+++ Highlighter.java
@@ -1,29 +1,40 @@
-import javax.xml.stream.*;
-import java.io.FileInputStream;
-import java.io.IOException;
+import org.w3c.dom.*;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.*;
 
 public class Highlighter
- {  
+ {   
+    
+    //Code Refactored From https://mkyong.com/java/how-to-modify-xml-file-in-java-dom-parser/
+    public static void main(String[] args) {
 
-    // Code Refactored From:  https://stackoverflow.com/questions/5059224/which-is-the-best-library-for-xml-parsing-in-java 
-    public static void parse(String filename) throws XMLStreamException, IOException {
-        try (FileInputStream fis = new FileInputStream(filename)) {
-            XMLInputFactory xmlInFact = XMLInputFactory.newInstance();
-            XMLStreamReader reader = xmlInFact.createXMLStreamReader(fis);
-            while(reader.hasNext()) {
-                reader.next(); // do something here
+    DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+
+    try (InputStream is = new FileInputStream("com.dropbox.android.xml")) {
+
+        DocumentBuilder db = dbf.newDocumentBuilder();
+
+        Document doc = db.parse(is);
+
+        //Get Nodes In Document
+        NodeList listOfNodes = doc.getElementsByTagName("node");
+
+        //Go Through Each Node and Change Value of Selected from F to T
+        for (int i = 0; i < listOfNodes.getLength(); i++) {
+            Node node = listOfNodes.item(i);
+            if (node.getNodeType() == Node.ELEMENT_NODE) {
+                Node selected_state = node.getAttributes().getNamedItem("selected");
+                if ("false".equals(selected_state.getTextContent())) {
+                        selected_state.setTextContent("true");
+                }   
             }
         }
+    }catch(Exception e){
+        e.printStackTrace();
     }
-
-    public static void main(String[] args){
-        try{
-        parse("com.dropbox.android.xml");
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-
     }
-   
 }
+
+
